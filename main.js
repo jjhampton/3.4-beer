9d0cd9ac795c8cd578aad4657364e32c955d8635
Jacob Smith <jacob@jacobsmith.io>
2015-05-21 10:26:49 -0400

    Implement fetchJSON

--- main.js
+++ main.js
@@ -2,3 +2,25 @@
   'use strict';
 
 })();
+
+/*
+  Call this function with the URL where the JSON lives.
+  We will pass a function as the second argument.
+  That function will be called when the request finished.
+  The argument to that function will be the JSON data.
+  You will need to change the values for url.
+*/
+function fetchJSON(url, callback){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(JSON.parse(xmlhttp.responseText));
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function logData(data) {
+  console.log(data);
+}
