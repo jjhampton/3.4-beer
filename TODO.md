bed30775fa750df946d7c6116306626aa03dfda8
Jacob Smith <jacob@jacobsmith.io>
2015-05-21 12:43:56 -0400

    Use handlebars to display characters

--- TODO.md
+++ TODO.md
@@ -3,4 +3,4 @@
 - [ ] Display a list of characters on the page
   - [x] Extract an array of character names
   - [x] Log each character name
-  - [ ] Display each character name in a `ul`
+  - [x] Display each character name in a `ul`
--- main.js
+++ main.js
@@ -4,63 +4,43 @@
   var url = 'http://gateway.marvel.com/v1/public/characters?apikey=82c9ab5aa3ff71aea506863fcf801efa';
   fetchJSON(url, app);
 
-/*
-  Call this function with the URL where the JSON lives.
-  We will pass a function as the second argument.
-  That function will be called when the request finished.
-  The argument to that function will be the JSON data.
-  You will need to change the values for url.
-*/
+  /*
+    Call this function with the URL where the JSON lives.
+    We will pass a function as the second argument.
+    That function will be called when the request finished.
+    The argument to that function will be the JSON data.
+    You will need to change the values for url.
+  */
 
-var ulElement = document.querySelector('.characters');
+  var ulElement = document.querySelector('.characters');
 
-function fetchJSON(url, callback){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function(){
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-            callback(JSON.parse(xmlhttp.responseText));
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
+  function fetchJSON(url, callback){
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function(){
+          if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+              callback(JSON.parse(xmlhttp.responseText));
+          }
+      };
+      xmlhttp.open("GET", url, true);
+      xmlhttp.send();
+  }
 
-/*
-  Etsy's API return data in a slightly different format.
-  Extract the data accordingly
-*/
-function app(response) {
-  var characters = response.data.results;
-  logCharacterNames(characters);
-}
+  /*
+    Etsy's API return data in a slightly different format.
+    Extract the data accordingly
+  */
+  function app(response) {
+    var characters = response.data.results;
+    console.log(characters);
+    displayCharacterNames(characters);
+  }
 
-function logCharacterNames(characters) {
-  listItemsForName(
-    extractNames(characters)
-  )
-  .forEach(displayListItem);
-}
-
-function listItemsForName(names) {
-  return names.map(function(name){
-    var li = document.createElement('li');
-    li.textContent = name;
-    return li;
-  });
-}
-
-function displayListItem(li) {
-  ulElement.appendChild(li);
-}
-
-function extractNames(characters) {
-  return characters.map(function(c){
-    return c.name;
-  });
-}
-
-function log() {
-  console.log.apply(console, arguments);
-}
-
+  function displayCharacterNames(characters) {
+    var source   = document.querySelector("#character-template").innerHTML;
+    var template = Handlebars.compile(source);
+    characters.forEach(function(character){
+      var output = template(character);
+      ulElement.insertAdjacentHTML('beforeend', output);
+    });
+  }
 })();
